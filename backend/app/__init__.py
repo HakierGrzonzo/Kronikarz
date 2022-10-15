b64d16ed133aa55530b0afa9a8414f763a9768b7
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2022-10-15 16:45:26 +0200

    Postawienie szkieletu aplikacji (#8)
    Sprawdzcie czy możecie to uruchomić
    
    # Wymagania:
    
    - docker
    - npm dla frontendu
    - [poetry](https://python-poetry.org/) dla backendu
    - system unixowy lub unixo-podobny (linux/macOS), jak ktoś ma windowsa
    to instaluje WSL2
    
    # Jak devować:
    
    ## frontend:
    
    - w jednej konsoli wystukaj `docker-compose up --build backend db`
    - W drugiej konsoli (w folderze `frontend/`) wystukaj `npm install &&
    npm run openapi && npm run dev`
    
    ## backend:
    
    - w jednej konsoli wystukaj `docker-compose up --build db`
    - w drugiej konsoli (w folderze `backend/`) wystukaj `poetry install &&
    poetry run uvicorn app:app --host 0.0.0.0 --reload`

---
+++ backend/app/__init__.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.params import Depends
+from surrealdb.clients.http import HTTPClient
+from .surreal_orm import get_db
+
+app = FastAPI()
+
+
+@app.get("/api/")
+async def hello(db: HTTPClient = Depends(get_db)):
+    return await db.select_all("test")
---
+++ backend/app/surreal_orm/__init__.py
@@ -0,0 +1,12 @@
+from os import environ
+from surrealdb.clients import HTTPClient
+
+
+def get_db() -> HTTPClient:
+    return HTTPClient(
+        f"http://{environ.get('DATABASE', 'localhost:8001')}",
+        namespace="test",
+        database="test",
+        username="root",
+        password="root",
+    )
