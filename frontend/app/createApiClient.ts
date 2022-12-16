1418524114ab89add607abf535e8d4ee824a76c8
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2022-12-16 22:03:31 +0100

    Add env configuration for backend

--- frontend/app/createApiClient.ts
+++ frontend/app/createApiClient.ts
@@ -2,7 +2,7 @@ import { AppClient } from "./client";
 
 export function createApiClient(token?: string): AppClient {
   return new AppClient({
-    BASE: "http://localhost:8000",
+    BASE: process.env.BACKEND ?? "http://localhost:8000",
     TOKEN: token,
   });
 }
