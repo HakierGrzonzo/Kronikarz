b5222765362922d5f0703dcc56ed5949554355a9
Wojciech Jasiewicz <pedrevsky@gmail.com>
2022-12-23 16:35:42 -0500

    zmiana nazwy drzewa

--- backend/app/trees.py
+++ backend/app/trees.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi_users import FastAPIUsers
@@ -42,7 +43,9 @@ def get_tree_router(fastapi_users: FastAPIUsers) -> APIRouter:
             raise HTTPException(403)
         return await session.Tree.select_deep(tree_id, ["nodes"])
 
-    @router.post("/delete", )
+    @router.post(
+        "/{tree_id}/delete",
+    )
     async def delete_tree(
         tree_id: str,
         current_user: UserRead = Depends(fastapi_users.current_user()),
@@ -51,8 +54,25 @@ def get_tree_router(fastapi_users: FastAPIUsers) -> APIRouter:
         """Moj pierwszy endpoint 8)"""
         if tree_id not in current_user.trees:
             raise HTTPException(403)
-        await session.User.patch(current_user.id, trees=[tree for tree in current_user.trees if tree != tree_id])
-        await session.Tree.delete(tree_id)
+        await asyncio.gather(
+            session.User.patch(
+                current_user.id,
+                trees=[tree for tree in current_user.trees if tree != tree_id],
+            ),
+            session.Tree.delete(tree_id),
+        )
+
+    @router.post("/{tree_id}/rename", response_model=Tree)
+    async def rename_tree(
+        tree_id: str,
+        new_name: str,
+        current_user: UserRead = Depends(fastapi_users.current_user()),
+        session: Session = Depends(get_db),
+    ):
+        if tree_id not in current_user.trees:
+            raise HTTPException(403)
+        tree = await session.Tree.patch(tree_id, name=new_name)
+        await session.commit()
+        return tree
 
     return router
-
