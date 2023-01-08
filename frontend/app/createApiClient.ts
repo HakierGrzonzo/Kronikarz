5e01807dcea29ac1bd090083bcd638220646456b
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2023-01-08 21:39:51 +0100

    Add field set deletion

--- frontend/app/createApiClient.ts
+++ frontend/app/createApiClient.ts
@@ -1,6 +1,19 @@
 import { AppClient } from "./client";
+import { decode, JwtPayload } from "jsonwebtoken";
+import { isPast, millisecondsInSecond } from "date-fns";
+import { redirect } from "@remix-run/node";
+import { cookieMaker, deleteCookie } from "./utils/cookieUtils";
 
 export function createApiClient(token?: string): AppClient {
+  if (token) {
+    const payload = decode(token, { complete: true })?.payload as JwtPayload;
+    const expiryDate = new Date(millisecondsInSecond * (payload.exp as number));
+    if (isPast(expiryDate)) {
+      throw redirect("/logout", {
+        headers: cookieMaker([deleteCookie("token")]),
+      });
+    }
+  }
   return new AppClient({
     BASE: process.env.BACKEND ?? "http://localhost:8000",
     TOKEN: token,
--- frontend/app/routes/editor/$treeID/fieldSets.tsx
+++ frontend/app/routes/editor/$treeID/fieldSets.tsx
@@ -1,8 +1,12 @@
 import {
   Accordion,
   AccordionSummary,
+  Alert,
+  AlertTitle,
   Box,
+  Button,
   IconButton,
+  Modal,
   Stack,
   Typography,
 } from "@mui/material";
@@ -16,8 +20,15 @@ import {
   ActionFunction,
 } from "@remix-run/node";
 import { FieldSetTemplate } from "~/client";
-import { useLoaderData, useSubmit } from "@remix-run/react";
+import {
+  useActionData,
+  useLoaderData,
+  useSubmit,
+  useTransition,
+} from "@remix-run/react";
 import { Delete, ExpandMore } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import Loader from "~/components/Loader";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const token = getCookie(request, "token");
@@ -45,28 +56,82 @@ export const action: ActionFunction = async ({ request }) => {
 export default function FieldSets() {
   const fieldSets: FieldSetTemplate[] = useLoaderData();
   const submit = useSubmit();
+  const [currentlyDeletedField, setCurrentlyDeletedField] = useState<
+    FieldSetTemplate | undefined
+  >(undefined);
+  const lastDeletedField = useActionData();
+  useEffect(() => setCurrentlyDeletedField(undefined), [lastDeletedField]);
+  const transition = useTransition();
   return (
-    <Stack sx={{ padding: 1 }} spacing={2} alignItems="flex-start">
-      <Typography variant="h3">My Field Sets</Typography>
-      <Stack sx={{ minWidth: "15cm" }}>
-        {fieldSets.map((set) => (
-          <Accordion key={set.id}>
-            <AccordionSummary expandIcon={<ExpandMore />}>
-              <Typography sx={{ flex: 1 }} variant="h6">
-                {set.name}
-              </Typography>
-              <IconButton
+    <>
+      <Modal open={!!currentlyDeletedField}>
+        <Alert
+          severity="error"
+          variant="filled"
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            padding: 1,
+          }}
+        >
+          <AlertTitle>Are you sure?</AlertTitle>
+          <Typography>
+            You are about to delete field set{" "}
+            <em>{currentlyDeletedField?.name}</em>. This action will also delete{" "}
+            <strong>All Data Stored In Those Fields!</strong>
+          </Typography>
+          {transition.state === "submitting" ? (
+            <Loader />
+          ) : (
+            <Stack direction="row" spacing={2} sx={{ paddingTop: 2 }}>
+              <Button
+                color="inherit"
+                variant="outlined"
+                onClick={() => setCurrentlyDeletedField(undefined)}
+              >
+                Cancel
+              </Button>
+              <Button
+                color="inherit"
+                variant="outlined"
                 onClick={() =>
-                  submit({ id: set.id }, { method: "post", replace: true })
+                  submit(
+                    { id: currentlyDeletedField?.id as string },
+                    { replace: true, method: "post" }
+                  )
                 }
               >
-                <Delete />
-              </IconButton>
-            </AccordionSummary>
-          </Accordion>
-        ))}
+                Delete
+              </Button>
+            </Stack>
+          )}
+        </Alert>
+      </Modal>
+      <Stack sx={{ padding: 1 }} spacing={2} alignItems="flex-start">
+        <Typography variant="h3">My Field Sets</Typography>
+        <Stack sx={{ minWidth: "15cm" }}>
+          {fieldSets.map((set) => (
+            <Accordion key={set.id}>
+              <AccordionSummary expandIcon={<ExpandMore />}>
+                <Typography sx={{ flex: 1 }} variant="h6">
+                  {set.name}
+                </Typography>
+                <IconButton
+                  onClick={(e) => {
+                    setCurrentlyDeletedField(set);
+                    e.stopPropagation();
+                  }}
+                >
+                  <Delete />
+                </IconButton>
+              </AccordionSummary>
+            </Accordion>
+          ))}
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 }
 
