f65776a51ff672294296110935e558f32f676c8e
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2023-01-09 20:28:21 +0100

    Fix wierd mui errors

---
+++ frontend/app/styles/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#A08D8D",
+    },
+    secondary: {
+      main: "#888798",
+    },
+    background: {
+      default: "#E0EEED",
+    },
+  },
+});
